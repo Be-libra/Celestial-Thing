2b3371c120f2ccd9bc93028063e6027d9516935d
Be-libra <50490129+Be-libra@users.noreply.github.com>
2019-06-11 02:02:06 +0530

    1  folder is uploaded

---
+++ Planetscomponent/Planets.js
@@ -0,0 +1,93 @@
+import React from 'react';
+
+
+const Planets= () =>{
+
+	return(
+			<div >
+			<article className="athelas">
+				  <div className="vh-100 dt w-100 tc  bg-dark-gray black cover " style={{background:'url(https://cdn.mos.cms.futurecdn.net/XFL6HquyTYfQH5L7ztttp3.jpg) no-repeat center'}}>
+				    <div className="dtc  v-mid">
+				      <h1 className="f1 f-headline-l fw1 i white-60">Planets</h1>
+				    </div>
+				  </div>
+				  <div className="center measure-wide f5 pv5 lh-copy ph2">
+				    <h1 className="f1 lh-title">...the life of the planet began the long, slow process of 
+				    modulating and regulating the physical conditions of the planet.</h1>
+				    
+
+				    <h1 className='f1 lh-title'>
+				    	What Is A Planet?
+
+				    </h1>
+				    <p>
+				    	This seemingly simple question doesn't have a simple answer. Everyone knows 
+				    	that Earth, Mars and Jupiter are planets. But both Pluto and Ceres were once 
+				    	considered planets until new discoveries triggered scientific debate about how 
+				    	to best describe them—a vigorous debate that continues to this day. The most 
+				    	recent definition of a planet was adopted by the International Astronomical 
+				    	Union in 2006. It says a planet must do three things:  
+				    </p>
+				    <p>
+				    	1.It must orbit a star (in our cosmic neighborhood, the Sun).
+				    </p> 
+
+				    <p>
+				    	2.It must be big enough to have enough gravity to force it into a spherical shape.
+				    </p>
+
+				  	<p>
+				      It must be big enough that its gravity cleared away any other objects of a similar 
+				      size near its orbit around the Sun.
+
+				    </p>
+
+				    <img src='http://www.gurumed.org/wp-content/uploads/2013/06/Plante-habitable-Gliese667C.jpg' alt='star life'/>
+				    <h1 className='f1 lh-title'>
+				    	The New Definition Of Planet
+				    </h1>
+
+				     <p>
+				     	Contemporary observations are changing our understanding of planetary systems, 
+				     	and it is important that our nomenclature for objects reflect our current 
+				     	understanding. This applies, in particular, to the designation "planets". 
+				     	The word "planet" originally described "wanderers" that were known only as 
+				     	moving lights in the sky. Recent discoveries lead us to create a new definition, 
+				     	which we can make using currently available scientific information.
+				     </p>
+
+				    <p>
+				    	The IAU therefore resolves that planets and other bodies, except satellites, in 
+				    	our Solar System be defined into three distinct categories in the following way:
+				    </p>
+
+				    <p>
+				    	1.A planet is a celestial body that 
+				    	(a) is in orbit around the Sun, 
+				    	(b) has sufficient mass for its self-gravity to overcome rigid body forces so 
+				    	that it assumes a hydrostatic equilibrium (nearly round) shape, and 
+				    	(c) has cleared the neighbourhood around its orbit.
+				    </p> 
+
+				    <p>
+				    	2.A "dwarf planet" is a celestial body that (a) is in orbit around the Sun,
+				    	 (b) has sufficient mass for its self-gravity to overcome rigid body forces 
+				    	 so that it assumes a hydrostatic equilibrium (nearly round) shape, (c) 
+				    	 has not cleared the neighbourhood around its orbit, and (d) is not a satellite.
+				    </p>
+
+				  	<p>
+				      All other objects,except satellites, orbiting the Sun shall be referred to 
+				      collectively as "Small Solar System Bodies".
+
+				    </p>
+				    <img src='https://upload.wikimedia.org/wikipedia/commons/5/59/Affiliated_Planets_in_Yemeni_Astrology.png' alt='planets' />
+				   	<img src='https://usercontent1.hubstatic.com/12575598_f520.jpg' alt='planets' />
+				   	<img src='https://thumbs-prod.si-cdn.com/JAsi4gQ6jgo2o5X5u6VQsWurLDU=/fit-in/1600x0/https://public-media.si-cdn.com/filer/bc/7c/bc7c0da6-f879-4a81-abbb-560753f216b7/celestia_nine_planets.png' alt='planet grid' />
+				    </div>
+				</article>
+			</div>	
+		);
+}
+
+export default Planets;
